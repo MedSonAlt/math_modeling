853e37197bf664e0e2c28d0af6199859f72c60e3
MedSonAlt <146106110+MedSonAlt@users.noreply.github.com>
2023-12-19 15:21:18 +0000

    gftyhg

--- lab_6_task3.py
+++ lab_6_task3.py
@@ -2,18 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
  
 def elipse_plotter():
-    a = 5
-    b = 2
-    x = np.arange(-3, 2, 0.1)
-    y = np.arange(-1, 2, 0.1)
- 
+    a = 0.5
+    b = 0.2
+    x = np.linspace(-1, 1, 50)
+    y = np.linspace(-1, 1, 50)
     X, Y = np.meshgrid(x, y)
- 
-    elips = x**2/a**2 + y**2/b**2 == 1
- 
+    elips = (X**2)/(a**2) + (Y**2)/(b**2) - 1
     plt.contour(X, Y, elips, levels=[0])
-
+    plt.axis("equal")
     plt.savefig("fig_task3.png")
+    
 
 if __name__ == "__main__":
     elipse_plotter()
